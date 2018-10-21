a58da927061bb4aadf16e21a0a65d16c25cb13aa
nihanimnihan <nihanimnihan@gmail.com>
2018-10-21 17:17:27 +0300

    Creating entities. Closes #3.

---
+++ src/main/java/nihanim/springframework/spring5recipeapp/domain/Category.java
@@ -0,0 +1,15 @@
+package nihanim.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+}
---
+++ src/main/java/nihanim/springframework/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,6 @@
+package nihanim.springframework.spring5recipeapp.domain;
+
+public enum Difficulty {
+
+    EASY, MODERATE, HARD
+}
